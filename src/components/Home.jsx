7b5b38362ea0fc36b28f96acd2cfdc4078fab2ed
jhachanda <jha95375@gmail.com>
2024-10-13 00:23:49 +0530

    Home Page

---
+++ src/components/Home.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import logo from "../assets/logo.png"
+
+function Home(){
+    return(
+        <>
+            <div>
+                <span className="flex flex-col">
+                    <span>
+                        <img className="scale-[150%]" src={logo} alt="" />
+                    </span>
+                    <span>
+                       Secure Hash
+                    </span>
+                </span>
+                <span>
+
+                </span>
+            </div>
+        </>
+    );
+
+}
+export default Home;
