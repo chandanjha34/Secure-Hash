2a8df8933bd02957534bdb32783359baa9b33e80
jhachanda <jha95375@gmail.com>
2024-10-16 02:58:29 +0530

    Timeline section created

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,26 +1,145 @@
 import React from "react";
 import gsap from "gsap";
-import Timeline1a from '../assets/Timeline1a.png'
+import Timeline1a from '../assets/Timeline/Timeline1a.png'
+import Timeline1b from '../assets/Timeline/Timeline1b.png'
+import Timeline1c from '../assets/Timeline/Timeline1c.png'
+import bullet from '../assets/Timeline/bullet.png'
  
 function Timeline(){
 
 
     return(
-        <div className="w-full h-screen text-white">
-            <div className="w-[140px] h-[50px] text-[150%] translate-x-[45vw] flex justify-center item-center rounded-full text-white font-bold translate-y-[20px] bg-[rgba(39,41,42,0.8)] shadow-lg">
+        <div className=" h-screen text-white ">
+            <div className="w-[140px] h-[50px] text-[150%] translate-x-[45vw] flex justify-center items-center rounded-full text-white font-bold translate-y-[20px] bg-[rgba(39,41,42,0.8)] shadow-lg">
                 <p>TIMELINE</p>
             </div>
-            <div>
-                <div className=" flex flex-col gap-[20px] translate-y-[20vh] rounded-lg translate-x-[25vw] w-[50vw] h-[60vh] bg-[rgba(39,41,42,0.9)]">
+            <div className="flex gap-[10vw] overflow-x-hidden">
+                <div className=" flex flex-col gap-[20px] translate-y-[18vh] rounded-lg translate-x-[25vw]  w-[50vw] h-[64vh] h-max-[64vh] bg-[rgba(39,41,42,0.9)] ">
                     <p className="flex justify-center font-bold text-[140%]">Birth of Blockchain: A Cryptographic Breakthrough</p>
-                    <p className="font-bold text-[120%]">1991: A Groundbreaking Discovery</p>
-                    <p>In 1991, Stuart Haber and W. Scott Stornetta proposed a secure chain of blocks for time-stamping digital documents.
-                        This innovation addressed concerns over digital document fraud.
-                        It introduced the core principles of transparency, security, and immutability.</p>
-                    <div>
-                        <img src={Timeline1a} alt="" />
-                        <img src="" alt="" />
-                        <img src="" alt="" />
+                    <p className="flex justify-center font-bold text-[120%]">1991: A Groundbreaking Discovery</p>
+                    <p className="translate-x-[20px]">
+                        <ul className="list-none">
+                            <li className="flex flex-center item-center"> <img src={bullet} className="w-4 h-4 mr-2 translate-y-[5px]" alt="" /> In 1991, Stuart Haber and W. Scott Stornetta proposed a secure chain of blocks for time-stamping digital documents.</li>
+                            <li className="flex flex-center item-center"> <img src={bullet} className="w-4 h-4 mr-2 translate-y-[5px]" alt="" /> This innovation addressed concerns over digital document fraud. </li>
+                            <li className="flex flex-center item-center"> <img src={bullet} className="w-4 h-4 mr-2 translate-y-[5px]" alt="" /> It introduced the core principles of transparency, security, and immutability.</li>
+                        </ul>
+                    </p>
+                    <div className="flex scale-[25%] translate-x-[-25vh] translate-y-[-44vh]">
+                        <img src={Timeline1a} className="" alt="" />
+                        <img src={Timeline1b} className="" alt="" />
+                        <img src={Timeline1c} className="" alt="" />
+                    </div>
+                </div>              
+                <div className=" flex flex-col gap-[20px] translate-y-[18vh] rounded-lg translate-x-[25vw] w-[50vw] h-[64vh] bg-[rgba(39,41,42,0.9)] ">
+                    <p className="flex justify-center font-bold text-[140%]">Birth of Blockchain: A Cryptographic Breakthrough</p>
+                    <p className="flex justify-center font-bold text-[120%]">1991: A Groundbreaking Discovery</p>
+                    <p className="translate-x-[20px]">
+                        <ul className="list-none">
+                            <li className="flex flex-center item-center"> <img src={bullet} className="w-4 h-4 mr-2 translate-y-[5px]" alt="" /> In 1991, Stuart Haber and W. Scott Stornetta proposed a secure chain of blocks for time-stamping digital documents.</li>
+                            <li className="flex flex-center item-center"> <img src={bullet} className="w-4 h-4 mr-2 translate-y-[5px]" alt="" /> This innovation addressed concerns over digital document fraud. </li>
+                            <li className="flex flex-center item-center"> <img src={bullet} className="w-4 h-4 mr-2 translate-y-[5px]" alt="" /> It introduced the core principles of transparency, security, and immutability.</li>
+                        </ul>
+                    </p>
+                    <div className="flex scale-[25%] translate-x-[-25vh] translate-y-[-44vh]">
+                        <img src={Timeline1a} className="" alt="" />
+                        <img src={Timeline1b} className="" alt="" />
+                        <img src={Timeline1c} className="" alt="" />
+                    </div>
+                </div>              
+                <div className=" flex flex-col gap-[20px] translate-y-[18vh] rounded-lg translate-x-[25vw] w-[50vw] h-[64vh] bg-[rgba(39,41,42,0.9)] ">
+                    <p className="flex justify-center font-bold text-[140%]">Birth of Blockchain: A Cryptographic Breakthrough</p>
+                    <p className="flex justify-center font-bold text-[120%]">1991: A Groundbreaking Discovery</p>
+                    <p className="translate-x-[20px]">
+                        <ul className="list-none">
+                            <li className="flex flex-center item-center"> <img src={bullet} className="w-4 h-4 mr-2 translate-y-[5px]" alt="" /> In 1991, Stuart Haber and W. Scott Stornetta proposed a secure chain of blocks for time-stamping digital documents.</li>
+                            <li className="flex flex-center item-center"> <img src={bullet} className="w-4 h-4 mr-2 translate-y-[5px]" alt="" /> This innovation addressed concerns over digital document fraud. </li>
+                            <li className="flex flex-center item-center"> <img src={bullet} className="w-4 h-4 mr-2 translate-y-[5px]" alt="" /> It introduced the core principles of transparency, security, and immutability.</li>
+                        </ul>
+                    </p>
+                    <div className="flex scale-[25%] translate-x-[-25vh] translate-y-[-44vh]">
+                        <img src={Timeline1a} className="" alt="" />
+                        <img src={Timeline1b} className="" alt="" />
+                        <img src={Timeline1c} className="" alt="" />
+                    </div>
+                </div>              
+                <div className=" flex flex-col gap-[20px] translate-y-[18vh] rounded-lg translate-x-[25vw] w-[50vw] h-[64vh] bg-[rgba(39,41,42,0.9)] ">
+                    <p className="flex justify-center font-bold text-[140%]">Birth of Blockchain: A Cryptographic Breakthrough</p>
+                    <p className="flex justify-center font-bold text-[120%]">1991: A Groundbreaking Discovery</p>
+                    <p className="translate-x-[20px]">
+                        <ul className="list-none">
+                            <li className="flex flex-center item-center"> <img src={bullet} className="w-4 h-4 mr-2 translate-y-[5px]" alt="" /> In 1991, Stuart Haber and W. Scott Stornetta proposed a secure chain of blocks for time-stamping digital documents.</li>
+                            <li className="flex flex-center item-center"> <img src={bullet} className="w-4 h-4 mr-2 translate-y-[5px]" alt="" /> This innovation addressed concerns over digital document fraud. </li>
+                            <li className="flex flex-center item-center"> <img src={bullet} className="w-4 h-4 mr-2 translate-y-[5px]" alt="" /> It introduced the core principles of transparency, security, and immutability.</li>
+                        </ul>
+                    </p>
+                    <div className="flex scale-[25%] translate-x-[-25vh] translate-y-[-44vh]">
+                        <img src={Timeline1a} className="" alt="" />
+                        <img src={Timeline1b} className="" alt="" />
+                        <img src={Timeline1c} className="" alt="" />
+                    </div>
+                </div>              
+                <div className=" flex flex-col gap-[20px] translate-y-[18vh] rounded-lg translate-x-[25vw] w-[50vw] h-[64vh] bg-[rgba(39,41,42,0.9)] ">
+                    <p className="flex justify-center font-bold text-[140%]">Birth of Blockchain: A Cryptographic Breakthrough</p>
+                    <p className="flex justify-center font-bold text-[120%]">1991: A Groundbreaking Discovery</p>
+                    <p className="translate-x-[20px]">
+                        <ul className="list-none">
+                            <li className="flex flex-center item-center"> <img src={bullet} className="w-4 h-4 mr-2 translate-y-[5px]" alt="" /> In 1991, Stuart Haber and W. Scott Stornetta proposed a secure chain of blocks for time-stamping digital documents.</li>
+                            <li className="flex flex-center item-center"> <img src={bullet} className="w-4 h-4 mr-2 translate-y-[5px]" alt="" /> This innovation addressed concerns over digital document fraud. </li>
+                            <li className="flex flex-center item-center"> <img src={bullet} className="w-4 h-4 mr-2 translate-y-[5px]" alt="" /> It introduced the core principles of transparency, security, and immutability.</li>
+                        </ul>
+                    </p>
+                    <div className="flex scale-[25%] translate-x-[-25vh] translate-y-[-44vh]">
+                        <img src={Timeline1a} className="" alt="" />
+                        <img src={Timeline1b} className="" alt="" />
+                        <img src={Timeline1c} className="" alt="" />
+                    </div>
+                </div>              
+                <div className=" flex flex-col gap-[20px] translate-y-[18vh] rounded-lg translate-x-[25vw] w-[50vw] h-[64vh] bg-[rgba(39,41,42,0.9)] ">
+                    <p className="flex justify-center font-bold text-[140%]">Birth of Blockchain: A Cryptographic Breakthrough</p>
+                    <p className="flex justify-center font-bold text-[120%]">1991: A Groundbreaking Discovery</p>
+                    <p className="translate-x-[20px]">
+                        <ul className="list-none">
+                            <li className="flex flex-center item-center"> <img src={bullet} className="w-4 h-4 mr-2 translate-y-[5px]" alt="" /> In 1991, Stuart Haber and W. Scott Stornetta proposed a secure chain of blocks for time-stamping digital documents.</li>
+                            <li className="flex flex-center item-center"> <img src={bullet} className="w-4 h-4 mr-2 translate-y-[5px]" alt="" /> This innovation addressed concerns over digital document fraud. </li>
+                            <li className="flex flex-center item-center"> <img src={bullet} className="w-4 h-4 mr-2 translate-y-[5px]" alt="" /> It introduced the core principles of transparency, security, and immutability.</li>
+                        </ul>
+                    </p>
+                    <div className="flex scale-[25%] translate-x-[-25vh] translate-y-[-44vh]">
+                        <img src={Timeline1a} className="" alt="" />
+                        <img src={Timeline1b} className="" alt="" />
+                        <img src={Timeline1c} className="" alt="" />
+                    </div>
+                </div>              
+                <div className=" flex flex-col gap-[20px] translate-y-[18vh] rounded-lg translate-x-[25vw] w-[50vw] h-[64vh] bg-[rgba(39,41,42,0.9)] ">
+                    <p className="flex justify-center font-bold text-[140%]">Birth of Blockchain: A Cryptographic Breakthrough</p>
+                    <p className="flex justify-center font-bold text-[120%]">1991: A Groundbreaking Discovery</p>
+                    <p className="translate-x-[20px]">
+                        <ul className="list-none">
+                            <li className="flex flex-center item-center"> <img src={bullet} className="w-4 h-4 mr-2 translate-y-[5px]" alt="" /> In 1991, Stuart Haber and W. Scott Stornetta proposed a secure chain of blocks for time-stamping digital documents.</li>
+                            <li className="flex flex-center item-center"> <img src={bullet} className="w-4 h-4 mr-2 translate-y-[5px]" alt="" /> This innovation addressed concerns over digital document fraud. </li>
+                            <li className="flex flex-center item-center"> <img src={bullet} className="w-4 h-4 mr-2 translate-y-[5px]" alt="" /> It introduced the core principles of transparency, security, and immutability.</li>
+                        </ul>
+                    </p>
+                    <div className="flex scale-[25%] translate-x-[-25vh] translate-y-[-44vh]">
+                        <img src={Timeline1a} className="" alt="" />
+                        <img src={Timeline1b} className="" alt="" />
+                        <img src={Timeline1c} className="" alt="" />
+                    </div>
+                </div>              
+                <div className=" flex flex-col gap-[20px] translate-y-[18vh] rounded-lg translate-x-[25vw] w-[50vw] h-[64vh] bg-[rgba(39,41,42,0.9)] ">
+                    <p className="flex justify-center font-bold text-[140%]">Birth of Blockchain: A Cryptographic Breakthrough</p>
+                    <p className="flex justify-center font-bold text-[120%]">1991: A Groundbreaking Discovery</p>
+                    <p className="translate-x-[20px]">
+                        <ul className="list-none">
+                            <li className="flex flex-center item-center"> <img src={bullet} className="w-4 h-4 mr-2 translate-y-[5px]" alt="" /> In 1991, Stuart Haber and W. Scott Stornetta proposed a secure chain of blocks for time-stamping digital documents.</li>
+                            <li className="flex flex-center item-center"> <img src={bullet} className="w-4 h-4 mr-2 translate-y-[5px]" alt="" /> This innovation addressed concerns over digital document fraud. </li>
+                            <li className="flex flex-center item-center"> <img src={bullet} className="w-4 h-4 mr-2 translate-y-[5px]" alt="" /> It introduced the core principles of transparency, security, and immutability.</li>
+                        </ul>
+                    </p>
+                    <div className="flex scale-[25%] translate-x-[-25vh] translate-y-[-44vh]">
+                        <img src={Timeline1a} className="" alt="" />
+                        <img src={Timeline1b} className="" alt="" />
+                        <img src={Timeline1c} className="" alt="" />
                     </div>
                 </div>              
             </div>
